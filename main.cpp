52c0fd85bf56d6fa1d3d7cc7d4a85dc339c6f6bc
Alnya <69311410+Alnya@users.noreply.github.com>
2021-07-10 00:43:12 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ using vi = vector<int>;
 using vvi = vector<vi>;
 using vs = vector<string>;
 using vvs = vector<vs>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
 
 int main() {
     ll ans = 0;
